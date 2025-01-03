e0aab491ae5dd35764675327849ff1dc007fbf2a
Rohit Joshi <rohitjoshi4353@gmail.com>
2025-01-03 15:20:05 +0530

    Update LaunchQuite.java

--- src/test/java/RevisePro/RevisionProject/LaunchQuite.java
+++ src/test/java/RevisePro/RevisionProject/LaunchQuite.java
@@ -3,27 +3,19 @@ package RevisePro.RevisionProject;
 import java.time.Duration;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
-import org.testng.annotations.Parameters;
+
 
 @Listeners(ListenersPro.class)
 public class LaunchQuite extends ListenersPro{
 	
-	@Parameters("browser")
 	@BeforeMethod
-	public void setUp(String reqBrowser) {
-		
-		if(reqBrowser.equals("Edge")) {
-			driver=new EdgeDriver();
-		}
-		
-		if(reqBrowser.equals("chrome")) {
+	public void setUp() {
 			driver=new ChromeDriver();
-			}
 		driver.get("https://katalon-demo-cura.herokuapp.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
